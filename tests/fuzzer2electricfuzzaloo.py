dadd5d6bfca45336c7ccc8fc3da36384f99ff9a1
mhoosen <mhoosen@student.wethinkcode.co.za>
2018-07-18 09:26:05 +0200

    oiahrg

--- tests/fuzzer2electricfuzzaloo.py
+++ tests/fuzzer2electricfuzzaloo.py
@@ -2,39 +2,9 @@
 
 import subprocess
 import signal
-from random import randrange
+from random import randrange, choice
 from sys import argv
-
-def do_test(data):
-	proc = subprocess.Popen(
-		argv[1],
-		stdin=subprocess.PIPE,
-		stdout=subprocess.PIPE,
-		stderr=subprocess.PIPE,
-	);
-	try:
-		stdout, stderr = proc.communicate(input=TEMPLATE, timeout=0.5)
-	except subprocess.TimeoutExpired:
-		proc.kill()
-		return None
-	sig = signal.Signals(-proc.returncode).name if proc.returncode < 0 else None
-	return stdout, stderr, sig
-
-def fuzz_data_del(data):
-	i = randrange(0, len(data))
-	return data[:i] + data[i + 1:]
-
-def fuzz_data_change(data):
-	i = randrange(0, len(data))
-	return data[:i] + bytes([randrange(0, 256)]) + data[i + 1:]
-
-def on_death(ret, stdin):
-	print(f"\nProgram killed by {ret[2]}")
-	with open('stdin.txt', 'wb') as f:
-		f.write(stdin)
-	print("You can reproduce this by running:")
-	print(f"{argv[1]} < stdin.txt")
-	exit()
+import re
 
 TEMPLATE = (
 	"$$$ exec p1 : [garbage]\n"
@@ -63,28 +33,61 @@ TEMPLATE = (
 	".....\n"
 ).encode('ascii')
 
+stdout_re = re.compile(r'-?\d+ -?\d+$')
+
+
+def do_test(data):
+	proc = subprocess.Popen(
+		argv[1],
+		stdin=subprocess.PIPE,
+		stdout=subprocess.PIPE,
+		stderr=subprocess.PIPE,
+	);
+	try:
+		stdout, stderr = proc.communicate(input=TEMPLATE, timeout=0.5)
+	except subprocess.TimeoutExpired:
+		proc.kill()
+		return None
+	sig = signal.Signals(-proc.returncode).name if proc.returncode < 0 else None
+	return stdout, stderr, sig
+
+
+def fuzz_data_del(data):
+	i = randrange(0, len(data))
+	return data[:i] + data[i + 1:]
+
+
+def fuzz_data_change(data):
+	i = randrange(0, len(data))
+	return data[:i] + bytes([randrange(0, 256)]) + data[i + 1:]
+
+
+def on_death(ret, stdin):
+	print(f"\nProgram killed by {ret[2]}")
+	with open('stdin.txt', 'wb') as f:
+		f.write(stdin)
+	print("You can reproduce this by running:")
+	print(f"{argv[1]} < stdin.txt")
+	exit()
+
+
 if len(argv) != 2:
 	print(f"Usage: {argv[0]} /path/to/filler")
 	exit(1)
 
-#TODO: gradually modify TEMPLATE until invalid stdout or crash
-
-print("Testing deleting random chars...")
-for i in range(10000):
-	stdin = fuzz_data_del(TEMPLATE)
-	ret = do_test(stdin)
-	if ret and ret[2]:
-		on_death(ret, stdin)
+stdin = TEMPLATE
+i = 0
+while 1:
 	print(f"\rRan {i + 1} tests", end='')
-print()
-
-print("Testing changing random chars...")
-for i in range(10000):
-	stdin = fuzz_data_change(TEMPLATE)
+	i += 1
+	stdin = choice((fuzz_data_del, fuzz_data_change))(stdin)
 	ret = do_test(stdin)
-	if ret and ret[2]:
+	if not ret:
+		stdin = TEMPLATE
+		continue
+	if ret[2] or not stdout_re.match(ret[0].decode('ascii')):
 		on_death(ret, stdin)
-	print(f"\rRan {i + 1} tests", end='')
+	if not ret[0].strip():
+		stdin = TEMPLATE
+		continue
 print()
-
-print("Everything looks OK!")
